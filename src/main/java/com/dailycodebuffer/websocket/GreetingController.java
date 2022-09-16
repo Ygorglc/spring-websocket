c0745dc221d200a34038e50d73924aa0c0a7f38b
ygorglc <ygorglc@gmail.com>
2022-09-16 16:32:27 -0300

    feature: implemetação concluida

--- src/main/java/com/dailycodebuffer/websocket/GreetingController.java
+++ src/main/java/com/dailycodebuffer/websocket/GreetingController.java
@@ -1,18 +1,25 @@
 package com.dailycodebuffer.websocket;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
+
 public class GreetingController {
 
     @MessageMapping("/hello")
+
     @SendTo("/topic/greetings")
-    public Greeting greet(HelloMessage message) throws InterruptedException {
-        Thread.sleep(2000);
-        return new Greeting("Hello, " +
-                HtmlUtils.htmlEscape(message.getName()));
+
+    public String greeting(@Payload String message) throws Exception {
+
+        Thread.sleep(1000); // simulated delay
+
+        return message;
+
     }
+
 }
